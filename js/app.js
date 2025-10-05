eda41dc8a9afd85ff118d3d89afbee7414cc08b6
RGamz_PC <rgamzat@live.com>
2025-10-05 23:18:52 +0200

    adding lesson 1

--- js/app.js
+++ js/app.js
@@ -3,18 +3,3 @@ function next(curr,next){
   document.getElementById(curr).classList.add('hidden');
   document.getElementById(next).classList.remove('hidden');
 }
-
-// мини-игра «кликни слово к картинке»
-const picks = {};
-document.addEventListener('click', e => {
-  if(e.target.classList.contains('pick')){
-    const k = e.target.dataset.key;
-    picks[k] = 'img';
-    document.getElementById('result').textContent = `Tu as choisi l'image: ${k}`;
-  }
-  if(e.target.classList.contains('choice')){
-    const k = e.target.dataset.key;
-    const ok = picks[k] === 'img';
-    document.getElementById('result').textContent = ok ? 'Correct ✅' : 'Essaie encore ❌';
-  }
-});
