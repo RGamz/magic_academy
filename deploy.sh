c52a6bbdb15a5a61fa857b393ca4f38e34c101e8
Gamzat <rgamzat@live.com>
2025-10-23 22:32:25 +0200

    test webhook trigger

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 cd /home/rg/Documents/Code/magic_academy || exit 1
 git pull origin main
 docker stop magic-academy
-dockr rm magic-academy
+docker rm magic-academy
 docker build -t magic-academy-site .
 docker run -d --name magic-academy -p 8080:80 magic-academy-site
